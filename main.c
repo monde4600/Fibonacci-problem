2490ad258081c66fe218f41e884e7df9e3cd05fc
Abanob Ade <104032534+monde4600@users.noreply.github.com>
2023-01-01 19:29:58 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int fib(unsigned int n)
     else
     {
         ve=2;
-        int arr[3] = {1,1};
+        int arr[2] = {1,1};
         for(int i=2;i<n;i++)
         {
             arr[i%2] = arr[(i)%2]+arr[(i-1)%2];
