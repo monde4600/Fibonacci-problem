413c0d4f641695b85980e1f488d6c4d6d50d44c4
Abanob Ade <104032534+monde4600@users.noreply.github.com>
2023-01-01 19:29:20 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+int fib(unsigned int n)
+{
+    int ve = 0;
+    if(!n) ;
+    else if(n<=2)   ve = 1;
+    else
+    {
+        ve=2;
+        int arr[3] = {1,1};
+        for(int i=2;i<n;i++)
+        {
+            arr[i%2] = arr[(i)%2]+arr[(i-1)%2];
+            ve = arr[i%2];
+        }
+    }
+    return ve;
+}
+int main()
+{
+    printf("%d",fib(10));
+
+    return 0;
+}
